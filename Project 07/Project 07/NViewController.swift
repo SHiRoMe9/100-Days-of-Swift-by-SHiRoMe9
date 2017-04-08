2af977a8bf98713c98d7769e8018e769a0b5443a
SHiRoMe9 <shirotoki9@gmail.com>
2017-04-08 12:54:58 +0800

    Project 07

---
+++ Project 07/Project 07/NViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NViewController.swift
+//  Project 07
+//
+//  Created by SHiRoMe9 on 08/04/2017.
+//  Copyright © 2017 SHiRoMe9. All rights reserved.
+//
+
+import UIKit
+
+class NViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project 07/Project 07/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  Project 07
+//
+//  Created by SHiRoMe9 on 08/04/2017.
+//  Copyright © 2017 SHiRoMe9. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var textPass : String = ""
+    
+    @IBOutlet weak var detailText: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Second View"
+        detailText?.text = textPass
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
